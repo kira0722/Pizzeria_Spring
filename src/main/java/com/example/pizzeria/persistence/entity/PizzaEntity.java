b0dd24ca8c276482645bb031b5de413ca78502ee
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-04-24 11:21:24 -0500

    subiendo correccion de tipo de dato enviado a BD

--- src/main/java/com/example/pizzeria/persistence/entity/PizzaEntity.java
+++ src/main/java/com/example/pizzeria/persistence/entity/PizzaEntity.java
@@ -20,10 +20,10 @@ public class PizzaEntity {
     @Column(nullable = false, length = 30, unique = true)
     private String name;
 
-    @Column(nullable = false, length = 150, unique = true)
+    @Column(nullable = false, length = 150)
     private String description;
 
-    @Column(nullable = false, columnDefinition = "Decimal(5,2)", unique = true)
+    @Column(nullable = false, columnDefinition = "Decimal(5,2)")
     private Double price;
 
     @Column(columnDefinition = "TINYINT")
