81c7407f541564152b19e02046c9e93edef905a0
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-04-24 15:50:42 -0500

    subiendo metodos de busqueda por fechas y methodos, y se agrego la consulta interna sobre pizzas veganas

--- src/main/java/com/example/pizzeria/service/OrderService.java
+++ src/main/java/com/example/pizzeria/service/OrderService.java
@@ -5,12 +5,20 @@ import com.example.pizzeria.persistence.repository.OrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class OrderService {
 
     private final OrderRepository orderRepository;
+
+    private static final String DELIVERY = "D";
+    private static final String CARRYOUT = "C";
+    private static final String ON_SITE = "S";
+
 
     @Autowired
     public OrderService(OrderRepository orderRepository) {
@@ -21,5 +29,15 @@ public class OrderService {
         List<OrderEntity> orders = this.orderRepository.findAll();
         orders.forEach(o -> System.out.println(o.getCustomer().getName()));
         return orders;
+    }
+
+    public List<OrderEntity> getTodayOrders(){
+        LocalDateTime today = LocalDate.now().atTime(0, 0);
+        return this.orderRepository.findAllByDateAfter(today);
+    }
+
+    public List<OrderEntity> getOutsideOrders(){
+        List<String> methods = Arrays.asList(DELIVERY, CARRYOUT);
+        return this.orderRepository.findAllByMethodIn(methods);
     }
 }
