5069a9925ae4bb931c769f1d32226c4073b76d83
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-04-24 10:23:40 -0500

    subiendo la creacion de las entidades

---
+++ src/main/java/com/example/pizzeria/persistence/entity/OrderEntity.java
@@ -0,0 +1,32 @@
+package com.example.pizzeria.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "pizza_order")
+public class OrderEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_order", nullable = false, unique = true)
+    private Integer idOrder;
+
+    @Column(name = "id_customer", nullable = false, length = 15)
+    private String idCostumer;
+
+    @Column(nullable = false, columnDefinition = "DATETIME")
+    private LocalDateTime date;
+
+    @Column(nullable = false, columnDefinition = "DECIMAL(6,2)")
+    private Double total;
+
+    @Column(nullable = false, columnDefinition = "CHAR(1)")
+    private String method;
+
+    @Column(name = "additional_notes", length = 200)
+    private String additionalNotes;
+}
