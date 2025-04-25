a93dff687630e7518d39d2653c9e885b9db0d31f
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-04-25 10:36:20 -0500

    subiendo metodos de consultas realizadas con JPQL para los customers

---
+++ src/main/java/com/example/pizzeria/service/CustomerService.java
@@ -0,0 +1,22 @@
+package com.example.pizzeria.service;
+
+
+import com.example.pizzeria.persistence.entity.CustomerEntity;
+import com.example.pizzeria.persistence.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private final CustomerRepository customerRepository;
+
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public CustomerEntity findByPhone(String phone){
+        return this.customerRepository.findByPhone(phone);
+    }
+}
---
+++ src/main/java/com/example/pizzeria/web/controller/CustomerController.java
@@ -0,0 +1,27 @@
+package com.example.pizzeria.web.controller;
+
+import com.example.pizzeria.persistence.entity.CustomerEntity;
+import com.example.pizzeria.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+    @Autowired
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/phone/{phone}")
+    public ResponseEntity<CustomerEntity> findByPhone(@PathVariable String phone){
+        return ResponseEntity.ok(customerService.findByPhone(phone));
+    }
+}
