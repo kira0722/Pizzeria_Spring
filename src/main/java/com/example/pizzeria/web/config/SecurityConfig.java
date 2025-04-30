542189f18e41f0baa70150d53243bb668452e620
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-04-30 14:47:28 -0500

    configurando CORS para la seguridad de las api's, y configurando las reglas de acceso en los endpoints y httpMethods

--- src/main/java/com/example/pizzeria/web/config/SecurityConfig.java
+++ src/main/java/com/example/pizzeria/web/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.pizzeria.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,8 +15,11 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize ->
                         authorize
+                                .requestMatchers(HttpMethod.GET, "/api/pizzas/**").permitAll()
+                                .requestMatchers(HttpMethod.PUT).denyAll()
                                 .anyRequest()
                                 .authenticated())
                 .httpBasic(Customizer.withDefaults());
