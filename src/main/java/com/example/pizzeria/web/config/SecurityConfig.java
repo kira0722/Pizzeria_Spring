f0fb34de189000421a12a35327a68c9f065360d2
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-04-30 11:05:06 -0500

    Desactivando seguridad de crf, para en el futuro usar JWT

--- src/main/java/com/example/pizzeria/web/config/SecurityConfig.java
+++ src/main/java/com/example/pizzeria/web/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,6 +13,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
         httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authorize ->
                         authorize
                                 .anyRequest()
