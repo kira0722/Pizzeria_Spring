31945649fc4107817ae0f1075e6e4bbda305d400
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-05-02 11:23:14 -0500

    subiendo las auditorias de usuarios a la hora de modificar y crear

--- src/main/java/com/example/pizzeria/persistence/audit/AuditableEntity.java
+++ src/main/java/com/example/pizzeria/persistence/audit/AuditableEntity.java
@@ -3,7 +3,9 @@ package com.example.pizzeria.persistence.audit;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
+import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDateTime;
@@ -18,4 +20,12 @@ public class AuditableEntity {
     @Column(name = "modified_date")
     @LastModifiedDate
     private LocalDateTime modifiedDate;
+
+    @CreatedBy
+    @Column(name = "created_by")
+    private String createBy;
+
+    @LastModifiedBy
+    @Column(name = "modified_by")
+    private String modifiedBy;
 }
