25e60cb73b6febc7db33b213b8a463f854f328f7
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-05-02 09:16:00 -0500

    subiendo aplicacion de validacion con JWT con auth0

--- src/main/java/com/example/pizzeria/web/config/JwtUtil.java
+++ src/main/java/com/example/pizzeria/web/config/JwtUtil.java
@@ -2,6 +2,7 @@ package com.example.pizzeria.web.config;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -21,5 +22,26 @@ public class JwtUtil {
                 .withIssuedAt(new Date())
                 .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)))
                 .sign(ALGORITHM);
+    }
+
+    public boolean isValid(String jwt){
+        try{
+            JWT.require(ALGORITHM)
+                    .build()
+                    .verify(jwt);
+
+            return true;
+
+        }catch (JWTVerificationException e){
+            return false;
+        }
+
+    }
+
+    public String getUsernamme(String jwt){
+        return JWT.require(ALGORITHM)
+                .build()
+                .verify(jwt)
+                .getSubject();
     }
 }
