0adbddd35cc89ab597b6507e6157cb56b5a51701
kria0722 <samuel_quirama23211@elpoli.edu.co>
2025-05-02 08:27:56 -0500

    agregando la clase del cuerpo de la configuracion de JWT, y su libreria

---
+++ src/main/java/com/example/pizzeria/web/config/JwtUtil.java
@@ -0,0 +1,25 @@
+package com.example.pizzeria.web.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+@Component
+public class JwtUtil {
+
+    private static String SECRET_KEY = "pizza";
+
+    private static Algorithm ALGORITHM = Algorithm.HMAC256(SECRET_KEY);
+
+    public String create(String username){
+        return JWT.create()
+                .withSubject(username)
+                .withIssuer("pizza")
+                .withIssuedAt(new Date())
+                .withExpiresAt(new Date(System.currentTimeMillis() + TimeUnit.DAYS.toMillis(15)))
+                .sign(ALGORITHM);
+    }
+}
